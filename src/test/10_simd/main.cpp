978444a07cbc19b765799fe2768771c31f088033
John <yangtianzhuo970425@gmail.com>
2025-02-23 23:41:29 +1300

    Refactor SIMD operations in IArray and IArrayScalarMul; enhance performance with xsimd optimizations; update test cases for improved clarity and functionality

--- src/test/10_simd/main.cpp
+++ src/test/10_simd/main.cpp
@@ -2,28 +2,33 @@
 // Created by Admin on 23/02/2025.
 //
 
-#include <iostream>
+#include <MyGM/point.h>
 #include <MyGM/vec.h>
-#include <MyGM/scale.h>
+#include <iostream>
+
+// #include <MyGM/scale.h>
 
 using namespace My;
 using namespace std;
 
-int main(int argc, char* argv[])
-{
+int main(int argc, char* argv[]) {
+  vecf3 test;
   vecf4 a;
   vecf4 b;
   cin >> a >> b;
   cout << a + b << endl;
   cout << a.dot(b) << endl;
 
-  scalef4 sa;
-  scalef4 sb;
-  cin >> sa >> sb;
-  cout << sa * sb << endl;
-  cout << sa.inverse() << endl;
-  xsimd::batch<float, 4> test;
-
+  pointf4 p0, p1;
+  cin >> p0 >> p1;
+  p0 -= a;
+  cout << p0 - p1 << endl;
+  // scalef4 sa;
+  // scalef4 sb;
+  // cin >> sa >> sb;
+  // cout << sa * sb << endl;
+  // cout << sa.inverse() << endl;
+  // xsimd::batch<float, 4> test;
 
   return 0;
 }
