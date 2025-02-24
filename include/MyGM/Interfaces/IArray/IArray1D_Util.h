203c3bc7900b14809cc9a3e0c9829bdb053c2336
John <yangtianzhuo970425@gmail.com>
2025-02-24 20:00:05 +1300

    Enhance SIMD support in IArray and line intersection calculations; add vector swizzle and shuffle macros for optimized performance; implement SIMD-optimized min and max component functions

--- include/MyGM/Interfaces/IArray/IArray1D_Util.h
+++ include/MyGM/Interfaces/IArray/IArray1D_Util.h
@@ -20,19 +20,55 @@ struct IArray1D_Util
   static constexpr size_t N = Arg_N<ArgList>;
 
   inline const Impl abs() const noexcept {
-    Impl rst{};
-    for (size_t i = 0; i < N; i++)
-      rst[i] = std::abs((*this)[i]);
-    return rst;
+#ifdef USE_XSIMD
+    if constexpr (std::is_same_v<T, float> && N == 4)
+      return xsimd::abs(this->get_batch());
+#endif  // USE_XSIMD
+    {
+      Impl rst;
+      for (size_t i = 0; i < N; i++)
+        rst[i] = std::abs((*this)[i]);
+      return rst;
+    }
   }
 
   inline T& min_component() noexcept { return (*this)[min_dim()]; }
 
-  inline T min_component() const noexcept { return (*this)[min_dim()]; }
+  inline T min_component() const noexcept {
+#ifdef USE_XSIMD
+    if constexpr (std::is_same_v<T, float> && N == 4) {
+      // 5 instructions
+      const auto& s0 = this->get_batch();
+      auto s1 = VecSwizzle(s0, 1, 0, 3, 2);
+      auto s2 = _mm_min_ps(s0, s1);
+      auto s3 = VecSwizzle(s2, 2, 3, 0, 1);
+      auto s4 = _mm_min_ps(s2, s3);
+      return _mm_cvtss_f32(s4);
+      // slow
+      //return std::min(std::min((*this)[0], (*this)[1]), std::min((*this)[2], (*this)[3]));
+    } else
+#endif  // USE_XSIMD
+      return (*this)[min_dim()];
+  }
 
   inline T& max_component() noexcept { return (*this)[max_dim()]; }
 
-  inline T max_component() const noexcept { return (*this)[max_dim()]; }
+  inline T max_component() const noexcept {
+#ifdef USE_XSIMD
+    if constexpr (std::is_same_v<T, float> && N == 4) {
+      // 5 instructions
+      const auto& s0 = this->get_batch();
+      auto s1 = VecSwizzle(s0, 1, 0, 3, 2);
+      auto s2 = _mm_max_ps(s0, s1);
+      auto s3 = VecSwizzle(s2, 2, 3, 0, 1);
+      auto s4 = _mm_max_ps(s2, s3);
+      return _mm_cvtss_f32(s4);
+      // slow
+      //return std::max(std::max((*this)[0], (*this)[1]), std::max((*this)[2], (*this)[3]));
+    } else
+#endif  // USE_XSIMD
+      return (*this)[max_dim()];
+  }
 
   inline constexpr size_t min_dim() const noexcept {
     T minVal = (*this)[0];
@@ -59,17 +95,29 @@ struct IArray1D_Util
   }
 
   static const Impl min(const Impl& x, const Impl& y) noexcept {
-    Impl rst{};
-    for (size_t i = 0; i < N; i++)
-      rst[i] = std::min(x[i], y[i]);
-    return rst;
+#ifdef USE_XSIMD
+    if constexpr (std::is_same_v<T, float> && N == 4)
+      return xsimd::min(x, y);
+#endif  // USE_XSIMD
+    {
+      Impl rst;
+      for (size_t i = 0; i < N; i++)
+        rst[i] = std::min(x[i], y[i]);
+      return rst;
+    }
   }
 
   static const Impl max(const Impl& x, const Impl& y) noexcept {
-    Impl rst{};
-    for (size_t i = 0; i < N; i++)
-      rst[i] = std::max(x[i], y[i]);
-    return rst;
+#ifdef USE_XSIMD
+    if constexpr (std::is_same_v<T, float> && N == 4)
+      return xsimd::min(x, y);
+#endif  // USE_XSIMD
+    {
+      Impl rst;
+      for (size_t i = 0; i < N; i++)
+        rst[i] = std::max(x[i], y[i]);
+      return rst;
+    }
   }
 };
 }  // namespace My
